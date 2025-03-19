4a4bd1e4685ebf66c70eb50ffa7c2a55f7b8ca0b
Ubuntu <ubuntu@ip-172-31-6-14.ap-south-1.compute.internal>
2025-03-19 13:13:36 +0000

    Modified the doctype_events for better handling

--- blinkit_connector/doctype_events/quotation.py
+++ blinkit_connector/doctype_events/quotation.py
@@ -4,4 +4,6 @@ from blinkit_connector.blinkit_repository import BlinkitRepository
 def before_submit(doc, method=None):
     if any(i.get("blinkit_po") for i in doc.items):
         blinkit_po = doc.items[0].blinkit_po
-        BlinkitRepository().acknowledge_po(blinkit_po)
+        sync_doc, sync_doc_name = frappe.db.get_value("Blinkit PO Data", blinkit_po, ["sync_via", "sync_doc"])
+        if  sync_doc == "Quotation" and sync_doc_name == doc.name:
+            BlinkitRepository().acknowledge_po(blinkit_po)
--- blinkit_connector/doctype_events/sales_order.py
+++ blinkit_connector/doctype_events/sales_order.py
@@ -1,12 +1,16 @@
 import frappe
+from blinkit_connector.blinkit_repository import BlinkitRepository
 
 def after_insert(doc, method=None):
     if any(i.get("blinkit_po") for i in doc.items):
         blinkit_po = doc.items[0].blinkit_po
         if not doc.po_no:
             doc.po_no = frappe.db.get_value("Blinkit PO Data", {"name": blinkit_po}, fieldname="po_number")
+            doc.save()
     
 def before_submit(doc, method=None):
     if any(i.get("blinkit_po") for i in doc.items):
-        #submit ack if sync doc is sales order
-        return
+        blinkit_po = doc.items[0].blinkit_po
+        sync_doc, sync_doc_name = frappe.db.get_value("Blinkit PO Data", blinkit_po, ["sync_via", "sync_doc"])
+        if  sync_doc == "Sales Order" and sync_doc_name == doc.name:
+            BlinkitRepository().acknowledge_po(blinkit_po)
